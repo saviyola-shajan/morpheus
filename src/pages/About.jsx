4fc34aee4e83e68da01ab423efe3fc57a57e52cb
saviyola-shajan <saviyolashajan69@gmail.com>
2024-07-15 20:17:10 +0530

    about completed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,37 +1,155 @@
 function About() {
   return (
-    <div className="pt-24 text-white flex">
-      <div className="w-1/2"></div>
-      <div className="w-1/2 mr-60 mt-24 font-medium font-sans border border-red-600 rounded-xl p-8 tracking-wider mb-12">
-        <div className="font-semibold font text-6xl hover:text-red-700">
-          Hi...
+    <div>
+      <div className="pt-24 text-white flex flex-col lg:flex-row justify-between">
+        <div className="w-full lg:w-1/2 h-96 flex justify-center items-center pl-6 pr-4 m-12 mt-48">
+          <img
+            src="/_RL_9939.JPG"
+            alt="portrait"
+            className="w-full h-auto rounded-xl"
+          />
         </div>
-        <div className="font-medium text-2xl text-red-600">Murphy</div>
-        <div className="font-normal text-xl text-red-600">-Director</div>
-        A director is the creative visionary behind a film
-        or a play, responsible for interpreting the script and bringing it to
-        life on screen or stage. They work closely with actors, guiding their
-        performances to ensure that each scene aligns with the overall vision.
-        Directors also collaborate with various departments such as
-        cinematography, set design, and sound to create a cohesive and immersive
-        experience for the audience. Their role requires a blend of artistic
-        talent, leadership skills, and an in-depth understanding of the
-        technical aspects of production. Whether working on a blockbuster movie
-        or an intimate theater production, a directors influence is pivotal in
-        shaping the story and its impact on viewers. A director is the creative
-        visionary behind a film or a play, responsible for interpreting the
-        script and bringing it to life on screen or stage. They work closely
-        with actors, guiding their performances to ensure that each scene aligns
-        with the overall vision. Directors also collaborate with various
-        departments such as cinematography, set design, and sound to create a
-        cohesive and immersive experience for the audience. Their role requires
-        a blend of artistic talent, leadership skills, and an in-depth
-        understanding of the technical aspects of production. Whether working on
-        a blockbuster movie or an intimate theater production, a directors
-        influence is pivotal in shaping the story and its impact on viewers.
+        <div className="w-full lg:w-1/2 mr-10 mt-16 pr-24 font-medium font-sans mb-12">
+          <div className="font-semibold text-6xl">
+            Hi...{" "}
+            <span className="font-normal text-3xl hover:text-red-600">
+              I&apos;m
+            </span>
+          </div>
+          <br />
+          <div className="font-medium text-2xl text-red-600">
+            Murphy Devasy{" "}
+          </div>
+          <div className="font-normal text-xl text-red-600">-Director</div>
+          <br />
+          Director of the critically acclaimed Malayalam film Nalla Nilavulla
+          Rathri, produced by Sandra Thomas Productions. This film has garnered
+          significant recognition, with its song and trailer surpassing 3.5
+          million views on YouTube. Upon its release on Amazon, it quickly
+          entered the top 10 in India.
+          <br />
+          After earning a degree in Visual Communication from Hindustan College,
+          Chennai, in 2007, I accumulated seven years of experience working with
+          Malayalam channels, Star Sports, and various programs for CNBC
+          channels. This period honed my skills and laid a solid foundation for
+          my career in visual storytelling.
+          <br />
+          For the past 12 years, I have dedicated myself to the advertising
+          field, progressing from Associate Director to Director at a renowned
+          ad film production house. My extensive experience has led to the
+          establishment of Morpheus Ads, where our focus is on creating
+          top-quality advertisements.
+          <br />
+          At Morpheus Ads, we are a collective of young, creative minds with
+          substantial experience in both the film and ad film industries. Our
+          recent projects have not only met but exceeded client expectations,
+          bringing big smiles to their faces. We pride ourselves on our
+          innovative approach and our commitment to excellence in every project
+          we undertake.
+        </div>
       </div>
+      <div className="w-full  mt-8 flex justify-center items-center">
+  <div className="w-full max-w-screen-xl flex justify-center items-center mb-12 mt-20">
+    <div className="group relative mb-4 w-full lg:w-1/4 p-4">
+      <img
+        className="w-full h-auto lg:h-auto transition duration-300 transform group-hover:scale-110 rounded-lg"
+        src="/3D-logo.jpg"
+        alt="Image 1"
+      />
+      <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity flex justify-center items-center">
+        <p className="text-white text-center">Text for Image 1</p>
+      </div>
+    </div>
+    <div className="group relative mb-4 w-full lg:w-1/4 p-4">
+      <img
+        className="w-full h-auto lg:h-auto transition duration-300 transform group-hover:scale-110 rounded-lg"
+        src="/3D-logo.jpg"
+        alt="Image 2"
+      />
+      <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity flex justify-center items-center">
+        <p className="text-white text-center">Text for Image 2</p>
+      </div>
+    </div>
+    <div className="group relative mb-4 w-full lg:w-1/4 p-4">
+      <img
+        className="w-full h-auto lg:h-auto transition duration-300 transform group-hover:scale-110 rounded-lg"
+        src="/3D-logo.jpg"
+        alt="Image 3"
+      />
+      <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity flex justify-center items-center">
+        <p className="text-white text-center">Text for Image 3</p>
+      </div>
+    </div>
+    <div className="group relative mb-4 w-full lg:w-1/4 p-4">
+      <img
+        className="w-full h-auto lg:h-auto transition duration-300 transform group-hover:scale-110 rounded-lg"
+        src="/3D-logo.jpg"
+        alt="Image 4"
+      />
+      <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity flex justify-center items-center">
+        <p className="text-white text-center">Text for Image 4</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+
     </div>
   );
 }
 
 export default About;
+
+// function About() {
+//   return (
+//     <div className="relative w-full h-screen text-white">
+//       <img
+//         src="/_RL_9939.JPG"
+//         alt="portrait"
+//         className="object-cover w-full h-full"
+//       />
+//       <div className="absolute inset-0 flex flex-col items-center justify-center p-8 bg-black bg-opacity-50">
+//         <div className="text-center">
+//           <div className="font-semibold text-6xl mb-4">
+//             Hi...{" "}
+//             <span className="font-normal text-3xl hover:text-red-600">
+//               I&apos;m
+//             </span>
+//           </div>
+//           <div className="font-medium text-2xl text-red-600 mb-2">
+//             Murphy Devasy
+//           </div>
+//           <div className="font-normal text-xl text-red-600 mb-8">-Director</div>
+//           <div className="font-medium text-xl tracking-wider">
+//             Director of the critically acclaimed Malayalam film Nalla Nilavulla
+//             Rathri, produced by Sandra Thomas Productions. This film has
+//             garnered significant recognition, with its song and trailer
+//             surpassing 3.5 million views on YouTube. Upon its release on Amazon,
+//             it quickly entered the top 10 in India.
+//             <br />
+//             After earning a degree in Visual Communication from Hindustan
+//             College, Chennai, in 2007, I accumulated seven years of experience
+//             working with Malayalam channels, Star Sports, and various programs
+//             for CNBC channels. This period honed my skills and laid a solid
+//             foundation for my career in visual storytelling.
+//             <br />
+//             For the past 12 years, I have dedicated myself to the advertising
+//             field, progressing from Associate Director to Director at a renowned
+//             ad film production house. My extensive experience has led to the
+//             establishment of Morpheus Ads, where our focus is on creating
+//             top-quality advertisements.
+//             <br />
+//             At Morpheus Ads, we are a collective of young, creative minds with
+//             substantial experience in both the film and ad film industries. Our
+//             recent projects have not only met but exceeded client expectations,
+//             bringing big smiles to their faces. We pride ourselves on our
+//             innovative approach and our commitment to excellence in every
+//             project we undertake.
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default About;
