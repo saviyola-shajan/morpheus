0150d7784b45301274fdc146ba108711f331693b
saviyola-shajan <saviyolashajan69@gmail.com>
2025-03-14 21:19:09 +0530

    small changes

--- src/Components/SctickOnButtons.jsx
+++ src/Components/SctickOnButtons.jsx
@@ -1,5 +1,5 @@
-import whatsappIcon from "/images/whatsapp.jpg";
-import CallIcon from "/images/call.jpg";
+import whatsappIcon from "/images/whatsapp.png";
+import CallIcon from "/images/call.png";
 
 const StickOnButtons = () => {
 
@@ -8,19 +8,19 @@ const StickOnButtons = () => {
   return (
     <div className="fixed bottom-16 right-12 flex flex-col items-end space-y-6 z-50">
       <div className="flex flex-col items-end space-y-4">
-        <a
-          href="tel:+91 9995546077"
-          className="w-[52px] h-[52px] rounded-full flex items-center justify-center"
-        >
-          <img src={CallIcon} alt="Call Icon" className="rounded-full"/>
-        </a>
-        <a
+      <a
           href="https://wa.me/919995546077"
           target="_blank"
           rel="noopener noreferrer"
           className="w-14 h-14 rounded-full flex items-center justify-center"
         >
-          <img src={whatsappIcon} alt="WhatsApp Icon" className="rounded-full" />
+          <img src={whatsappIcon} alt="WhatsApp Icon" />
+        </a>
+        <a
+          href="tel:+91 9995546077"
+          className="w-14 h-14 rounded-full flex items-center justify-center"
+        >
+          <img src={CallIcon} alt="Call Icon"/>
         </a>
       </div>
     </div>
--- src/Components/VideoPlayer/videoPlayer.jsx
+++ src/Components/VideoPlayer/videoPlayer.jsx
@@ -3,13 +3,13 @@ import ReactPlayer from 'react-player';
 const videoPlayer = () => {
     const videos = [
         { id: 1, title: 'Video 1', url: 'https://youtu.be/2RyVzGqApd8?si=O5bvfAd4KrDgq9ek' },
-        { id: 2, title: 'Video 2', url: 'https://youtu.be/ArOImkUvguI?si=1vJPyI_FccRXOjJY' },
+        { id: 2, title: 'Video 2', url: 'https://youtu.be/cDYF5xRMHPY?si=wEXMk2XsPa--flwt' },
         { id: 3, title: 'Video 3', url: 'https://youtu.be/LCCTxYudmL4?si=DcXV1kzdjAbr9xrT' },
         { id: 4, title: 'Video 4', url: 'https://youtu.be/K7gCTuxgQ7Q?si=AeuEEPzUcTMsSFaI' },
         { id: 5, title: 'Video 5', url: 'https://youtu.be/rrtE3QMGbRM?si=V0TkloKNQHqMN7eR' },
         { id: 6, title: 'Video 6', url: 'https://youtu.be/L8pF1cxjhjI?si=PmnZglrPUlLdLSTt' },
         { id: 7, title: 'Video 7', url: 'https://youtu.be/4d62eXS0lxk?si=-eWZM2pEf5L73lAk' },
-        { id: 8, title: 'Video 8', url: 'https://youtu.be/cDYF5xRMHPY?si=wEXMk2XsPa--flwt' },
+        { id: 8, title: 'Video 8', url: 'https://youtu.be/ArOImkUvguI?si=1vJPyI_FccRXOjJY' },
         { id: 9, title: 'Video 9', url: 'https://youtu.be/O7g7AaylM24?si=AqQQxQZ4zHGEfJ7M' },
         { id: 10, title: 'Video 10', url: 'https://youtu.be/R6hyfs3E13c?si=Cnp3s_Myor2qO7sB' }
 
@@ -17,9 +17,9 @@ const videoPlayer = () => {
 
     ]
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 px-20 py-16 mt-28">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 px-20 py-16 mt-36">
       {videos.map(video => (
-        <div key={video.id} className="p-4 rounded-lg bg-white bg-opacity-15 backdrop-blur-md "
+        <div key={video.id} className="p-1 rounded-sm bg-[#FFFFFF]/10 backdrop-blur-md "
         >
           <div className='player-wrapper'
            style={{ height: '220px' }}>
